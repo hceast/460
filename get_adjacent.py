3945c91e017f73ba8cc9200f3ed6defd78d62dd2
igmcnelis <ig.mcnelis@gmail.com>
2018-10-22 08:28:53 -0500

    Fixed issue with get_adjacent. Using the acutal vertices with Lat/Long was problematic b/c they decimals weren't excatly matiching up.

--- get_adjacent.py
+++ get_adjacent.py
@@ -11,26 +11,20 @@ outputs of scipy.spatial's voronoi function (vor).
 
 def get_adjacent(point, vor):
 
-    #Create a nested list that stores the vertices for every Thiessen polygon
-    verts = vor.vertices
-    reg_verts_index = vor.regions
+    #Generate necessary lists for operations to be performed
+    reg = vor.point_region
+    input_points = vor.points
+    ridge = vor.ridge_points  
+    reg_verts_idx = vor.regions
     
     i = 0
-    j = 0
-    reg_verts = [[] for i in range(len(reg_verts_index))]
-    
-    for i  in range(len(reg_verts_index)):
-        if (-1 in reg_verts_index[i]):
-            reg_verts_index[i].remove(-1)
-        reg_verts[i] = [0 for i in range(len(reg_verts_index[i]))]
-        for j in range (len(reg_verts_index[i])):
-            reg_verts[i][j] = [verts[(reg_verts_index[i][j])][0], verts[(reg_verts_index[i][j])][1]]
+
+    for i  in range(len(reg_verts_idx)):
+        if (-1 in reg_verts_idx[i]):
+            reg_verts_idx[i].remove(-1)
     
     #Determine critical node information:
     #Locate the node the within the Voronoi diagram and save its index
-    input_points = vor.points
-    reg = vor.point_region
-    
     i = 0
     for i in range(len(input_points)):
         if (point[0] == input_points[i][0]) and (point[1] == input_points[i][1]):
@@ -42,13 +36,13 @@ def get_adjacent(point, vor):
     #Get the region of the node, i.e. the index of the point's Thiessen poly w/in the Voronoi diagram
     point_region = reg[point_index]
     
+    
     #Get the vertices of the region
-    point_region_verts = reg_verts[point_region]
+    pt_region_verts = reg_verts_idx[point_region]
     
     
     #Determine adjacencies:
     #Create a list that determines node adjacency based on Voronoi ridges
-    ridge = vor.ridge_points
     adj_points_ridge = []
     
     i = 0
@@ -71,17 +65,15 @@ def get_adjacent(point, vor):
     i = 0
     j = 0
     k = 0
-    for i in range(len(point_region_verts)):
-        vertex = (point_region_verts[i][0], point_region_verts[i][0]) 
-        for j in range(len(reg_verts)):
-            if (j != point_region):
-                if (len(reg_verts[j]) == 1):
-                    if (vertex[0] == reg_verts[j][0][0]) and (vertex[1] == reg_verts[j][0][1]):
-                            adj_reg_verts.append(j)
-                else:
-                    for k in range(len(reg_verts[j])):
-                        if (vertex[0] == reg_verts[j][k][0]) and (vertex[1] == reg_verts[j][k][1]):
-                            adj_reg_verts.append(j)
+    for i in range(len(pt_region_verts)):
+        vertex = pt_region_verts[i]
+        if (vertex != -1):
+            for j in range(len(reg_verts_idx)):
+                if (j != point_region):
+                    for k in range(len(reg_verts_idx[j])):
+                        if (reg_verts_idx[j][k] == vertex):
+                            if (j not in adj_reg_verts):
+                                adj_reg_verts.append(j)
     
     #Converts region back to points                        
     adj_pts_verts = []
@@ -93,17 +85,22 @@ def get_adjacent(point, vor):
             if (reg[j] == adj_reg_verts[i]):
                 adj_pts_verts.append(j)
                 break
-                
+    
     #Determine final node adjacency list:
-    adj_nodes = adj_pts_verts
+    adj_nodes = []
+    
+    i = 0
+    for i in range(len(adj_points_ridge)):
+        adj_nodes.append(adj_points_ridge[i])
+    
     #adj_points_ridge.append(100)
     
     i = 0
     j = 0
     
-    for i in range(len(adj_points_ridge)):
-        if (adj_points_ridge[i] not in adj_pts_verts):
-            adj_nodes.append(adj_points_ridge[i])
+    for i in range(len(adj_pts_verts)):
+        if (adj_pts_verts[i] not in adj_nodes):
+            adj_nodes.append(adj_pts_verts[i])
     
     return adj_nodes
     
